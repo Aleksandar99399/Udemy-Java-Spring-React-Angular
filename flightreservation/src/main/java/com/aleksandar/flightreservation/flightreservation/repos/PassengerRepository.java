609b2cfcd58247c63f75b08a4b5a95e227c08a08
Aleksandar Yunchov <aleksandar_unchev@abv.bg>
2021-07-04 16:30:01 +0300

    repair

--- flightreservation/src/main/java/com/aleksandar/flightreservation/flightreservation/repos/PassengerRepository.java
+++ flightreservation/src/main/java/com/aleksandar/flightreservation/flightreservation/repos/PassengerRepository.java
@@ -2,6 +2,8 @@ package com.aleksandar.flightreservation.flightreservation.repos;
 
 import com.aleksandar.flightreservation.flightreservation.entities.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PassengerRepository extends JpaRepository<Passenger,Long> {
 }
